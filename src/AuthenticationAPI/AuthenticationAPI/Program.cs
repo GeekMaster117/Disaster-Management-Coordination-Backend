adaf232ce41b9e23aa3bf0e3931ec8f4643a734e
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-08-31 15:20:31 +0530

    Updated .gitignore

--- src/AuthenticationAPI/AuthenticationAPI/Program.cs
+++ src/AuthenticationAPI/AuthenticationAPI/Program.cs
@@ -7,8 +7,3 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
