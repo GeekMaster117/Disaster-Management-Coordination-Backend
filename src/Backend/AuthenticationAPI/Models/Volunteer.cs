65c55f6b1abd78756215804f1a6b87806bd99d34
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-09-02 10:11:36 +0530

    Added Volunteer Model

---
+++ src/Backend/AuthenticationAPI/Models/Volunteer.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationAPI.Models
+{
+	public class Volunteer : IdentityUser
+	{
+		public bool isVolunteer {  get; set; }
+		public bool isEligible { get; set; }
+	}
+}
