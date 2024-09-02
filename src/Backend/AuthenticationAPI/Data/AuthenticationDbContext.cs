15f2c232583f871ba23e68bbf401bfeb87f1dd60
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-09-02 15:28:17 +0530

    Added some initial seed data

--- src/Backend/AuthenticationAPI/Data/AuthenticationDbContext.cs
+++ src/Backend/AuthenticationAPI/Data/AuthenticationDbContext.cs
@@ -8,5 +8,28 @@ namespace AuthenticationAPI.Data
 	public class AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
 		: IdentityDbContext<IdentityUser>(options)
 	{
+		protected virtual void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			builder.Entity<IdentityUser>().HasData(
+				new IdentityUser
+				{
+					Id = "1",
+					UserName = "admin",
+					PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "admin"),
+				});
+			builder.Entity<IdentityRole>().HasData(
+				new IdentityRole
+				{
+					Id = "1",
+					Name = AuthenticationAPI.Models.UserRoles.Admin
+				});
+			builder.Entity<IdentityUserRole<string>>().HasData(
+				new IdentityUserRole<string>
+				{
+					RoleId = "1",
+					UserId = "1"
+				});
+		}
 	}
 }
