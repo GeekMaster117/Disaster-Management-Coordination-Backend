2d9ae000f772ce362349a94ad94d77f6c30434f0
Rahul.Pashikanti <rahul.pashikanti@ust.com>
2024-09-02 14:57:10 +0530

    Registered the services inside the program.cs and jwt secret key to the configuration

--- src/Backend/AuthenticationAPI/Program.cs
+++ src/Backend/AuthenticationAPI/Program.cs
@@ -1,5 +1,9 @@
 using AuthenticationAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -8,8 +12,34 @@ builder.Services.AddDbContext<AuthenticationDbContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("UserDB"));
 });
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthenticationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+// Adding Jwt Bearer
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    };
+});
 
 var app = builder.Build();
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 
