702ca7e83420bf792161c4965f9f235ac58e06bc
Rahul.Pashikanti <rahul.pashikanti@ust.com>
2024-09-05 11:56:47 +0530

    Added LoginDTOValidator

---
+++ src/Backend/AuthenticationAPI/Validations/LoginDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using AuthenticationAPI.Models;
+
+namespace AuthenticationAPI.Validators
+{
+    public class LoginDTOValidator : AbstractValidator<LoginDTO>
+    {
+        public LoginDTOValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .Length(3, 20).WithMessage("Username must be between 3 and 20 characters.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+                .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one symbol.");
+        }
+    }
+}
