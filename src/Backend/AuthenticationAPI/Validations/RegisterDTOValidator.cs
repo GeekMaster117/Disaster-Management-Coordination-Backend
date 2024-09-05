8c23c93bf1484ff2cc068d5c4f895734d0f3ec96
Rahul.Pashikanti <rahul.pashikanti@ust.com>
2024-09-05 14:02:39 +0530

    Removed whitespaces validation rule from register validation

--- src/Backend/AuthenticationAPI/Validations/RegisterDTOValidator.cs
+++ src/Backend/AuthenticationAPI/Validations/RegisterDTOValidator.cs
@@ -23,8 +23,7 @@ namespace AuthenticationAPI.Validators
                 .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
                 .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
                 .Matches("[0-9]").WithMessage("Password must contain at least one number.")
-                .Matches("[`=;',.//~!@#$%^&*()_+{}:<>?]").WithMessage("Password must contain any special character")
-                .Matches(@"[\s\S]").WithMessage("Password cannot contain whitespace characters.");
+                .Matches("[`=;',.//~!@#$%^&*()_+{}:<>?]").WithMessage("Password must contain any special character");
         }
     }
 }
