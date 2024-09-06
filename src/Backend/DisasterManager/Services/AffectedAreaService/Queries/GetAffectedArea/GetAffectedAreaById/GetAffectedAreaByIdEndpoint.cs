8121b6a8983d9ab5c238b824b9c64bc3eb166ad5
Rahul.Pashikanti <rahul.pashikanti@ust.com>
2024-09-06 18:04:50 +0530

    Completed GetAffefctedAreaById endpoint

--- src/Backend/DisasterManager/Services/AffectedAreaService/Queries/GetAffectedArea/GetAffectedAreaById/GetAffectedAreaByIdEndpoint.cs
+++ src/Backend/DisasterManager/Services/AffectedAreaService/Queries/GetAffectedArea/GetAffectedAreaById/GetAffectedAreaByIdEndpoint.cs
@@ -1,6 +1,31 @@
-﻿namespace DisasterManager.Services.AffectedAreaService.Queries.GetAffectedArea.GetAffectedAreaById
+﻿using DisasterManager.Models;
+using Carter;
+using DisasterManager.Services.AffectedAreaService.Queries.GetAffectedArea.GetAffectedAreaById;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace DisasterManager.Services.AffectedAreaService.Queries.GetAffectedArea.GetAffectedAreaById
 {
-    public class GetAffectedAreaByIdEndpoint
+    public class GetAffectedAreaByIdEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/affectedarea/{id:int}", async (int id, IMediator mediator) =>
+            {
+                var response = await mediator.Send(new GetAffectedAreaByIdQuery { Id = id });
+                if (response == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Content(
+                    JsonSerializer.Serialize(response),
+                    "application/json",
+                    Encoding.UTF8
+                );
+            });
+        }
     }
 }
