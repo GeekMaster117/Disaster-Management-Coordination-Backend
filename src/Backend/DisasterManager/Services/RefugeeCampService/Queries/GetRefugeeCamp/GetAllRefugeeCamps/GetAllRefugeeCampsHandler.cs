9afed565a266ca52b77d50ea8077345f05417a92
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-09-07 12:31:56 +0530

    Created GetAllRefugeeCamps Service

---
+++ src/Backend/DisasterManager/Services/RefugeeCampService/Queries/GetRefugeeCamp/GetAllRefugeeCamps/GetAllRefugeeCampsHandler.cs
@@ -0,0 +1,24 @@
+﻿using DisasterManager.Data;
+using DisasterManager.Models;
+using Mapster;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DisasterManager.Services.RefugeeCampService.Queries.GetRefugeeCamp.GetAllRefugeeCamps
+{
+	public class GetAllRefugeeCampsHandler(DisasterManagerDbContext context) : IRequestHandler<GetAllRefugeeCampsCommand, ResponseDTO>
+	{
+		private readonly DisasterManagerDbContext _context = context;
+
+		public async Task<ResponseDTO> Handle(GetAllRefugeeCampsCommand request, CancellationToken cancellationToken)
+		{
+			List<GetRefugeeCampResponse> camps = [];
+			await _context.RefugeeCamps.ForEachAsync(camp => camps.Add(camp.Adapt<GetRefugeeCampResponse>()));
+			return new()
+			{
+				StatusCode = DefaultMessages.Success.StatusCode,
+				Message = camps
+			};
+		}
+	}
+}
