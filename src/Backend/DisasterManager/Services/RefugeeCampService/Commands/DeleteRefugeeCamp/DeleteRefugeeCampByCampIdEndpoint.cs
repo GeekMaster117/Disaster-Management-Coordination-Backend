6acc185de7e884b333f0407472d69b7fc82e3dda
Rahul.Pashikanti <rahul.pashikanti@ust.com>
2024-09-07 17:30:40 +0530

    Completed DeleteRefugeeCampByCampId Endpoint

---
+++ src/Backend/DisasterManager/Services/RefugeeCampService/Commands/DeleteRefugeeCamp/DeleteRefugeeCampByCampIdEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Carter;
+using DisasterManager.Services.RefugeeCampService.Commands.DeleteRefugeeCamp;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace DisasterManager.Services.RefugeeCampService.Commands.DeleteRefugeeCamp
+{
+    public class DeleteRefugeeCampByCampIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/refugeecamp", async ([FromBody] DeleteRefugeeCampByCampIdCommand command, IMediator mediator) =>
+            {
+                var response = await mediator.Send(command);
+
+                return Results.Content(
+                    JsonSerializer.Serialize(response),
+                    "application/json",
+                    Encoding.UTF8,
+                    response.StatusCode
+                );
+            });
+        }
+    }
+}
