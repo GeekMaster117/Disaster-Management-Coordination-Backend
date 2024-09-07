90ecfd91c832ff6a093e9cd5700e1ee7bb227179
Rahul.Pashikanti <rahul.pashikanti@ust.com>
2024-09-07 17:47:31 +0530

    Completed GetRefugeeCampByCampId Endpoint

--- src/Backend/DisasterManager/Services/RefugeeCampService/Queries/GetRefugeeCamp/GetRefugeeCampByCampId/GetRefugeeCampByCampIdEndpoint.cs
+++ src/Backend/DisasterManager/Services/RefugeeCampService/Queries/GetRefugeeCamp/GetRefugeeCampByCampId/GetRefugeeCampByCampIdEndpoint.cs
@@ -1,6 +1,29 @@
-﻿namespace DisasterManager.Services.RefugeeCampService.Queries.GetRefugeeCamp.GetRefugeeCampByCampId
+﻿using Carter;
+using DisasterManager.Services.RefugeeCampService.Queries.GetRefugeeCamp.GetRefugeeCampByCampId;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DisasterManager.Services.RefugeeCampService.Queries.GetRefugeeCamp.GetRefugeeCampByCampId
 {
-    public class GetRefugeeCampByCampIdEndpoint
+    public class GetRefugeeCampByCampIdEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/refugeecamp", async ([FromBody] GetRefugeeCampByCampIdCommand query, IMediator mediator) =>
+            {
+                var response = await mediator.Send(query);
+
+                return Results.Content(
+                    JsonSerializer.Serialize(response),
+                    "application/json",
+                    Encoding.UTF8,
+                    response.StatusCode
+                );
+            });
+        }
     }
 }
