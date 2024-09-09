58a6af7ae564e6da85def0de98956fdf9f4172e2
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-09-09 16:16:03 +0530

    Added CORS for all

--- src/Backend/DisasterManager/Program.cs
+++ src/Backend/DisasterManager/Program.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddMediatR(configuration =>
 {
 	configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
@@ -16,8 +16,16 @@ builder.Services.AddDbContext<DisasterManagerDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("MapDB"));
 });
 builder.Services.AddCarter();
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
+
+app.UseCors(configuration =>
+{
+	configuration.AllowAnyOrigin();
+	configuration.AllowAnyHeader();
+	configuration.AllowAnyMethod();
+});
 
 app.MapCarter();
 
--- src/Backend/Gateway/Program.cs
+++ src/Backend/Gateway/Program.cs
@@ -8,16 +8,21 @@ namespace Gateway
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
-            builder.Services.AddAuthorization();
 
+            builder.Services.AddCors();
             builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
             builder.Services.AddOcelot(builder.Configuration);
 
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
+
+            app.UseCors(configurePolicy =>
+            {
+                configurePolicy.AllowAnyOrigin();
+                configurePolicy.AllowAnyHeader();
+                configurePolicy.AllowAnyMethod();
+            });
 
             app.UseOcelot();
 
