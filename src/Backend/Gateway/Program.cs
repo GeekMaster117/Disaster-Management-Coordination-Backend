caf37e9a2db983252af1a756767e99146988445f
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-09-10 10:45:02 +0530

    Fixed ocelot not being to route signal r

--- src/Backend/Gateway/Program.cs
+++ src/Backend/Gateway/Program.cs
@@ -1,32 +1,26 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-namespace Gateway
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+builder.Services.AddCors();
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+
+app.UseCors(configurePolicy =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            // Add services to the container.
+    configurePolicy.WithOrigins("http://localhost:4200");
+    configurePolicy.AllowAnyHeader();
+    configurePolicy.AllowAnyMethod();
+    configurePolicy.AllowCredentials();
+});
 
-            builder.Services.AddCors();
-            builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-            builder.Services.AddOcelot(builder.Configuration);
+app.UseWebSockets();
 
-            var app = builder.Build();
-            // Configure the HTTP request pipeline.
+app.UseOcelot().Wait();
 
-            app.UseCors(configurePolicy =>
-            {
-                configurePolicy.AllowAnyOrigin();
-                configurePolicy.AllowAnyHeader();
-                configurePolicy.AllowAnyMethod();
-            });
-
-            app.UseOcelot();
-
-            app.Run();
-        }
-    }
-}
+app.Run();
